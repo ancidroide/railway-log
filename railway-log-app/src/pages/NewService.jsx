951b5e72840c29bf85bffff8c787f2e5e29ec4d2
ancidroide <ancipolla@gmail.com>
2026-03-01 12:39:25 +0100

    refactoring in progress

--- railway-log-app/src/pages/NewService.jsx
+++ railway-log-app/src/pages/NewService.jsx
@@ -1,75 +1,38 @@
 import { useState } from "react"
 import { DatePicker } from "@mui/x-date-pickers"
 import { TextField, Button, Box, Stack, Card, CardContent, Typography } from "@mui/material"
+import TrainForm from "../components/TrainForm"
 
 
 const NewService = () => {
-  const [trainForm, setTrainForm] = useState({ date: null, numero: '', partenza: '', arrivo: '', materiale: ''})
-  const [trains, setTrains] = useState(() => {
-    const saved = localStorage.getItem('trains')
-    return saved ? JSON.parse(saved) : []
-  })
-  
+  const [date, setDate] = useState(null)
+  const [selectedDate, setSelectedDate] = useState(null)
 
-  const handleAddTrain = (event) => {
-    event.preventDefault()
-    if (trainForm.date && trainForm.numero && trainForm.partenza && trainForm.arrivo && trainForm.materiale) {
-      const newTrain = {...trainForm, id: Date.now()}
-      const newTrains = [...trains, newTrain]
-
-      setTrains(newTrains)
-      localStorage.setItem('trains', JSON.stringify(newTrains))
-      setTrainForm({ date: null, numero: '', partenza: '', arrivo: '', materiale: ''})
-    }
+  const handleCreateNewService = () => {
+    setSelectedDate(date)
   }
-  return (
-    <Box p={2}>
-      <Stack spacing={2}>
-        <DatePicker 
-          label="Data servizio"
-          value={trainForm.date}
-          onChange={(newValue) => setTrainForm({...trainForm, date: newValue})}
-        />
 
-        <TextField
-          label="Numero treno"
-          value={trainForm.numero}
-          onChange={(e) => setTrainForm({...trainForm, numero: e.target.value})}
-        />
-        <TextField 
-          label="Partenza"
-          value={trainForm.partenza}
-          onChange={(e) => setTrainForm({...trainForm, partenza: e.target.value})}
-        />
-        <TextField
-          label="Arrivo"
-          value={trainForm.arrivo}
-          onChange={(e) => setTrainForm({...trainForm, arrivo: e.target.value})}
-        />
-        <TextField
-          label="Materiale rotabile"
-          value={trainForm.materiale}
-          onChange={(e) => setTrainForm({...trainForm, materiale: e.target.value})}
-        />
-        <Button variant="contained" onClick={handleAddTrain}>Aggiungi Treno</Button>
-      </Stack>
+  if (selectedDate) {
+    return (
+      <TrainForm 
+        selectedDate={selectedDate}
+      />
+    )
+  } else {
+      return (
+        <Box p={2}>
+          <Stack spacing={2}>
+            <DatePicker 
+              label="Data servizio"
+              value={date}
+              onChange={(newValue) => setDate(newValue)}
+            />
+          </Stack>
+          <Button onClick={handleCreateNewService}>Crea</Button>
+        </Box>
+      )
+  }
 
-      {trains.map(train =>
-        <Card key={train.id} sx={{ mb: 1 }}>
-          <CardContent>
-            <Typography variant="h6">
-              {train.numero}
-            </Typography>
-            <Typography>
-              {train.partenza} - {train.arrivo}
-            </Typography>
-          </CardContent>
-
-        </Card>
-      )}
-
-    </Box>
-  )
 }
 
 
